0ad5b8930cac4a0d1eb69f7353129a13b0ae0d50
iclectic <tansonestost@yahoo.com>
2024-10-02 21:44:51 +0100

    Bottom Tab Navigation Added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,19 @@
-import { Stack } from "expo-router"
+import { Tabs } from "expo-router"
+import Ionicons from '@expo/vector-icons/Ionicons';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import {theme} from "../theme"
 
 export default function Layout() {
     return (
-        <Stack>
-            <Stack.Screen name="index" options={{
+        <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorGreen }}> 
+            <Tabs.Screen name="index" options={{
                 title: "Home",
+                tabBarIcon: ({ size, color}) => <Ionicons name="leaf-sharp" size={size} color={color} />
             }} />
-        </Stack>
+            <Tabs.Screen name="profile" options={{
+                title: "Profile",
+                tabBarIcon: ({ size, color }) => <AntDesign name="user" size={size} color={color} />
+            }} />
+        </Tabs>
     )
 }
