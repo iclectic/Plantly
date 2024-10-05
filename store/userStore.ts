646a4d2049f827ea3d989eb6a51f05067a792355
iclectic <tansonestost@yahoo.com>
2024-10-05 08:39:28 +0100

    Implemented persistent onboarding status with zustand and AsyncStorage

--- store/userStore.ts
+++ store/userStore.ts
@@ -1,18 +1,26 @@
 import { create } from "zustand";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { persist, createJSONStorage} from "zustand/middleware"
+
 
 type UserStore = {
     hasFinishedOnboarding: boolean;
     toggleHadOnboarded: () => void;
 };
 
-export const useUserStore = create<UserStore>((set) => ({
-    hasFinishedOnboarding: false,
-    toggleHadOnboarded: () => {
-        set((state) => {
-            return {
+export const useUserStore = create
+    (persist<UserStore>((set) => ({
+        hasFinishedOnboarding: false,
+        toggleHadOnboarded: () => {
+            set((state) => {
+              return {
                 ...state,
                 hasFinishedOnboarding: !state.hasFinishedOnboarding,
             };
         });
     },
-}));
+  }), {
+    name: "plantly-user-store",
+    storage: createJSONStorage(() => AsyncStorage),
+  }),
+);
