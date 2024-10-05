048f0c50f1d01a27a7af065d3b0e272300b774e0
iclectic <tansonestost@yahoo.com>
2024-10-05 08:02:22 +0100

    store management using zustand

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,13 +2,17 @@ import { Tabs, Redirect } from "expo-router"
 import Ionicons from '@expo/vector-icons/Ionicons';
 import AntDesign from '@expo/vector-icons/AntDesign';
 import {theme} from "@/theme"
-
-const hasFinishedOnboarding = true;
+import { useUserStore} from "@/store/userStore";
 
 export default function Layout() {
+    const hasFinishedOnboarding = useUserStore(
+        (state) => state.hasFinishedOnboarding,
+    );
+    
     if (!hasFinishedOnboarding) {
-        return <Redirect href="onboarding" />
+        return <Redirect href="/onboarding" />;
     }
+
     return (
         <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorGreen }}> 
             <Tabs.Screen name="index" options={{
