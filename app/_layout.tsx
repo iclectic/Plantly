b14e41169facbc0b5dc9c5374c1ebb07f5d95843
iclectic <tansonestost@yahoo.com>
2024-12-23 14:02:31 +0000

    new plant form added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,7 @@ export default function Layout() {
     return (
         <Stack>
             <Stack.Screen name="(tabs)" options={{ headerShown: false, animation: "fade"}} />
-            <Stack.Screen name="onboarding" options={{ headerShown: false, animation: "fade"}} />
+            <Stack.Screen name="onboarding" options={{ headerShown: false, animation: "fade",}} />
             <Stack.Screen name="new" options={{ presentation: "modal", title: "New Plant" }} />
         </Stack>
     );
